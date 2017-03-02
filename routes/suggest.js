124044cac5b8ebc01b8b7bfd129b42564f04290f
kris-karr <kris.karr@gmail.com>
2017-03-01 20:06:55 -0800

    fix poll sort function

--- routes/suggest.js
+++ routes/suggest.js
@@ -4,23 +4,16 @@ var mongoClient = require('mongodb').MongoClient;
 var userUtils = require('../db_api/userUtils');
 var gameUtils = require('../db_api/gameUtils');
 
-var sortPollResults = function(a, b) {
-	if (a.results.length <= playerCount && b.results.length > playerCount) {
-		return 1;
-	}
-	if (b.results.length <= playerCount && a.results.length > playerCount) {
-		return -1;
-	}
-	if (b.results.length <= playerCount && a.results.length <= playerCount) {
+var sortPollsForPlayerCount = function(playerCount) {
+	return function(a, b) {
+		if (a.results[playerCount - 1].sub_results[0].num_votes > b.results[playerCount - 1].sub_results[0].num_votes) {
+			return -1;
+		}
+		if (a.results[playerCount - 1].sub_results[0].num_votes < b.results[playerCount - 1].sub_results[0].num_votes) {
+			return 1;
+		}
 		return 0;
 	}
-	if (a.results[playerCount - 1].sub_results[0].num_votes > b.results[playerCount - 1].sub_results[0].num_votes) {
-		return -1;
-	}
-	if (a.results[playerCount - 1].sub_results[0].num_votes < b.results[playerCount - 1].sub_results[0].num_votes) {
-		return 1;
-	}
-	return 0;
 }
 
 router.get('/', function(req, res, next) {
@@ -46,7 +39,7 @@ router.post('/', function(req, res, next) {
 		userUtils.getGameIdsFromUsernames(db, usernames, function(gameIds) {
 			gameUtils.getSuggestedPlayerPolls(db, gameIds, function(pollResults) {
 				// TODO: do smarter sorting
-				pollResults.sort(sortPollResults).slice(0, 10).forEach(pr => {
+				pollResults.filter(pr => pr.results.length > playerCount).sort(sortPollsForPlayerCount(playerCount)).slice(0, 10).forEach(pr => {
 					pageVars.suggestions.push(pr.name);
 				});
 				res.render('suggest', pageVars);
